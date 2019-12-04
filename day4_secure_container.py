26094f8a9c8b8c5c0d1d8a7fd963101eaec51b6b
junwoo77lee <47956198+junwoo77lee@users.noreply.github.com>
2019-12-04 00:49:36 -0700

    modified the day4 solution

--- day4_secure_container.py
+++ day4_secure_container.py
@@ -1,87 +1,39 @@
 # passcode range: 206938-679128
-import numpy as np
+from collections import Counter
 
 passcodes = range(206938, 679129)
 
 
+def rule_one(passcode: str) -> bool:
+    return len(set(passcode)) <= 5
+
+
+def rule_two(passcode: str) -> bool:
+    return passcode == ''.join(sorted(passcode))
+
+
+def rule_one_modified(passcode: str) -> bool:
+    counts = Counter(passcode)
+    return any(v == 2 for v in counts.values())
+
+
 def rule_check(passcode: str) -> bool:
-    check2, check3, check4 = False, False, True
-
-    passcode = [int(num) for num in passcode]
-    # print(passcode)
-    passcode1 = passcode[:-1]
-    passcode2 = passcode[1:]
-
-    a = np.array(passcode1)
-    b = np.array(passcode2)
-
-    diff = list(b - a)
-    # print(diff)
-    if len(list(filter(lambda x: x < 0, diff))) == 0:
-        check3 = True
-    else:
-        check3 = False
-
-    if len(list(filter(lambda x: x == 0, diff))) == 0:
-        check2 = False
-    else:
-        check2 = True
-
-    # rule check4
-    zeros_in_diff = [num for num in diff if num == 0]
-    if len(zeros_in_diff) == 0:
-        pass
-    else:
-        first_zero = diff.index(0)
-        if len(zeros_in_diff) == 1:
-            check4 = True
-        elif len(zeros_in_diff) == 2:
-            if diff[first_zero] == diff[first_zero + 1]:
-                check4 = False
-            else:
-                check4 = True
-        elif len(zeros_in_diff) == 3:
-            if diff[first_zero] != diff[first_zero + 1]:
-                check4 = True
-            else:
-                if diff[first_zero + 1] == diff[first_zero + 2]:
-                    check4 = False
-                else:
-                    check4 = True
-        elif len(zeros_in_diff) == 4:
-            if diff[first_zero] != diff[first_zero + 1]:
-                check4 = True
-            else:
-                if diff[first_zero + 1] == diff[first_zero + 2]:
-                    if diff[first_zero + 2] == diff[first_zero + 3]:
-                        check4 = False
-                    else:
-                        check4 = True
-                else:
-                    check4 = False
-        else:  # len(zeros_in_diff) == 5
-            check4 = False
-
-        # print(check1, check2, check3)
-    return all([check2, check3, check4])
+    return all([rule_one(passcode), rule_two(passcode)])
 
 
-# print(rule_check('112233'))
-# print(rule_check('123444'))
-# print(rule_check('111122'))
-# print(rule_check('111111'))
-# print(rule_check('223450'))
-# print(rule_check('123789'))
+def rule_check_modified(passcode: str) -> bool:
+    return all([rule_one_modified(passcode), rule_two(passcode)])
 
 
 def find_correct_passwords():
-    results = []
-    for passcode in passcodes:
-        passcode = str(passcode)
-        results.append(rule_check(passcode))
-
-    # print(results)
-    return sum(results)
+    return sum(rule_check(str(passcode)) for passcode in passcodes)
 
 
-print(find_correct_passwords())
+def find_correct_passwords_modified():
+    return sum(rule_check_modified(str(passcode)) for passcode in passcodes)
+
+
+part1 = find_correct_passwords()
+part2 = find_correct_passwords_modified()
+print(part1)
+print(part2)
